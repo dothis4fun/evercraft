ddb4777d88359b80e464456c106ab5428e047e4e
Steve Turley <steve@turleylabs.com>
2020-08-17 08:16:05 -0500

    Adding example of using webdriver

--- src/test/java/com/dojo/evercraft/cucumber/CharacterStepDefs.java
+++ src/test/java/com/dojo/evercraft/cucumber/CharacterStepDefs.java
@@ -3,11 +3,23 @@ package com.dojo.evercraft.cucumber;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class CharacterStepDefs {
     @Autowired
     BossServer bossServer;
+
+    @Autowired
+    Browser browser;
 
     @Given("a new game is started")
     public void aNewGameIsStarted() {
@@ -16,6 +28,11 @@ public class CharacterStepDefs {
 
     @When("I create a character")
     public void iCreateACharacter() {
+        WebDriver driver = browser.getWebDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.get("https://google.com/ncr");
+        driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+        WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3>div")));
     }
 
     @Then("my character shows up in the game")
